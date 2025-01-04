7d53a511ff753e0d85ce012565389577260cf5d6
김진호 <rlawlsgh1227@gmail.com>
2025-01-04 15:10:24 +0900

    feat: / 요청이 들어오면 /wiki/대문으로 redirect 하는 기능 추가

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,15 @@
 import type {NextConfig} from 'next';
+import URLS from '@/constants/urls';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  redirects: async () => [
+    {
+      source: '/',
+      destination: `${URLS.WIKI}/${URLS.DAEMOON}`,
+      permanent: false,
+    },
+  ],
 };
 
 export default nextConfig;
