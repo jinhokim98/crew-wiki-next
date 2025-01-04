bfb4600d371e0794adf16e0b3bcf3d8e73501f4d
김진호 <rlawlsgh1227@gmail.com>
2025-01-04 15:10:44 +0900

    chore: 이전 constants 내용 옮겨오기

---
+++ client/src/constants/keys.ts
@@ -0,0 +1,19 @@
+const QUERY = {
+  POST_DOCUMENT: 'post-document',
+  GET_DOCUMENT_BY_TITLE: 'get-document-by-title',
+  GET_RECENTLY_DOCUMENTS: 'get-recently-documents',
+  GET_DOCUMENT_LOGS: 'get-document-logs',
+  GET_SPECIFIC_DOCUMENT_LOG: 'get-specific-document-log',
+  GET_DOCUMENT_SEARCH: 'get-document-search',
+};
+
+const SESSION_STORAGE = {
+  WRITE: 'write-document',
+};
+
+const KEYS = {
+  QUERY,
+  SESSION_STORAGE,
+};
+
+export default KEYS;
---
+++ client/src/constants/urls.ts
@@ -0,0 +1,12 @@
+const URLS = {
+  MAIN: '/',
+  WIKI: '/wiki',
+  DOCS: ':title',
+  EDIT: '/edit',
+  POST: '/post',
+  DAEMOON: '대문',
+  LOGS: 'logs',
+  SPECIFIC_LOG: 'log/:logId',
+};
+
+export default URLS;
