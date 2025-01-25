9cf4cd841af6859a341d5757a61c0869af458738
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2025-01-25 12:00:52 +0900

    feat: 오류 페이지 마이그레이션 (#16)
    * fix: 편집로그도 마크다운 to html 적용
    
    * feat: 위키 문서 조회 실패 시 에러 fallback 구현

--- client/src/app/wiki/[title]/log/[logId]/page.tsx
+++ client/src/app/wiki/[title]/log/[logId]/page.tsx
@@ -2,6 +2,7 @@ import {getSpecificDocumentLog} from '@api/document';
 import DocumentContents from '@components/Document/DocumentContents';
 import DocumentFooter from '@components/Document/DocumentFooter';
 import DocumentHeader from '@components/Document/DocumentHeader';
+import markdownToHtml from '@utils/markdownToHtml';
 
 interface Props {
   params: {title: string; logId: string};
@@ -10,12 +11,13 @@ interface Props {
 const Page = async ({params}: Props) => {
   const {title, logId} = await params;
   const document = await getSpecificDocumentLog(Number(logId));
+  const contents = await markdownToHtml(document.contents);
 
   return (
     <section className="flex flex-col items-center w-full gap-6">
       <div className="flex flex-col gap-6 w-full h-fit bg-white border-primary-100 border-solid border rounded-xl p-8 max-[768px]:p-4 max-[768px]:gap-2 ">
         <DocumentHeader title={decodeURI(title)} />
-        <DocumentContents contents={document.contents} />
+        <DocumentContents contents={contents} />
       </div>
       <DocumentFooter generateTime={document.generateTime} />
     </section>
