415362bf57cc7f9dd613e90a9cb603e2afc5c495
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2025-02-07 13:03:43 +0900

    fix: 화면이 클 때 왼쪽으로 쏠려있는 현상 해결 (#40)

--- client/src/app/wiki/layout.tsx
+++ client/src/app/wiki/layout.tsx
@@ -7,10 +7,12 @@ const Layout = ({children}: React.PropsWithChildren) => {
     <div className="App relative">
       <AmplitudeInitializer />
       <WikiHeader />
-      <main className="flex items-start justify-center h-fit gap-6 py-6 px-4 max-w-[1440px] w-full max-[768px]:py-2 max-[768px]:px-0">
-        {children}
-        <RecentlyEdit />
-      </main>
+      <div className="flex items-center justify-center h-fit">
+        <main className="flex items-start justify-center h-fit gap-6 py-6 px-4 max-w-[1440px] w-full max-[768px]:py-2 max-[768px]:px-0">
+          {children}
+          <RecentlyEdit />
+        </main>
+      </div>
     </div>
   );
 };
