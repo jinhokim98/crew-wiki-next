56aeb63612b9b39bcf0cb4229ac70d73f1f4ec7c
김진호 <rlawlsgh1227@gmail.com>
2025-04-04 17:17:28 +0900

    test: 확인을 위한 변경

--- client/src/app/wiki/statistics/page.tsx
+++ client/src/app/wiki/statistics/page.tsx
@@ -2,7 +2,7 @@ const Page = () => {
   return (
     <section className="flex flex-col items-center w-full gap-6">
       <div className="flex flex-col gap-6 w-full h-fit min-h-[864px] bg-white border-primary-100 border-solid border rounded-xl p-8 max-md:p-4 max-md:gap-2">
-        <h1 className="font-bm text-2xl text-grayscale-800">테스트1</h1>
+        <h1 className="font-bm text-2xl text-grayscale-800">테스트</h1>
       </div>
     </section>
   );
