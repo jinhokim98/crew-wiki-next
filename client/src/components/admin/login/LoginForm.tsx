d174cbc15a1ddde9dc7724ea3edb8c3b98ee1b08
eunwoo-levi <eunwoo1341@gmail.com>
2025-05-27 00:39:26 +0900

    refactor: LoginForm 컴포넌트를 responsive 하게 리팩토링

--- client/src/components/admin/login/LoginForm.tsx
+++ client/src/components/admin/login/LoginForm.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Button from '@components/common/Button';
 import {useRouter} from 'next/navigation';
 import {useState} from 'react';
 
@@ -35,7 +36,7 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="w-full max-w-md rounded-lg bg-primary-200 px-8 py-10 shadow-lg">
+    <div className="w-[80%] max-w-md rounded-lg bg-primary-200 px-8 py-10 shadow-lg md:w-full">
       <h2 className="mb-6 text-2xl font-bold text-white">관리자 로그인</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
@@ -70,7 +71,8 @@ export default function LoginForm() {
         </div>
         <button
           type="submit"
-          className="w-full rounded bg-primary-600 px-4 py-2 font-semibold text-white transition duration-200 hover:bg-primary-700 focus:outline-none focus:ring focus:ring-primary-500"
+          className="w-full rounded bg-primary-400 px-4 py-2 font-semibold text-white transition duration-200 hover:bg-primary-700 focus:outline-none focus:ring focus:ring-primary-500 disabled:cursor-not-allowed disabled:bg-gray-300"
+          disabled={!adminForm.loginId || !adminForm.password}
         >
           로그인
         </button>
