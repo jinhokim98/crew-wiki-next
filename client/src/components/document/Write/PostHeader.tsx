3ebefc224a09eb3ae235ee8b5b0b4f3121ed916b
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2025-06-29 13:04:55 +0900

    refactor: 작성하기, 편집하기 페이지 리렌더링 제어 (#85)
    * chore: zustand 의존성 추가
    
    * chore: store 절대경로 추가
    
    * feat: 문서 입력 상태를 관리하는 document를 zustand로 관리
    
    * refactor: 문서 제목, 작성자, 내용별로 상태를 구독해 작성 중인 필드만 리렌더링 되도록 최적화
    
    * remove: 사용하지 않는 Context 삭제
    
    * style: http client 접미사 방식으로 변경
    
    * feat: 작성, 수정 페이지 이탈 시 form 리셋 되도록 설정
    
    * refactor: pathname에 의존하지 않고 prop으로 모드 전달
    
    * refactor: initial value 상수로 정의

--- client/src/components/document/Write/PostHeader.tsx
+++ client/src/components/document/Write/PostHeader.tsx
@@ -3,10 +3,53 @@
 import Button from '@components/common/Button';
 import DocumentTitle from '@components/document/layout/DocumentTitle';
 import {useRouter} from 'next/navigation';
-import {useDocumentWriteContext} from '@context/DocumentWriteContext';
+import {ExcludeImages, useDocument} from '@store/document';
+import {uploadImages} from '@apis/images';
+import {replaceLocalUrlToS3Url} from '@utils/replaceLocalUrlToS3Url';
+import {PostDocumentContent} from '@apis/document';
+import {getBytes} from '@utils/getBytes';
+import {usePostDocument} from '@hooks/mutation/usePostDocument';
+import {usePutDocument} from '@hooks/mutation/usePutDocument';
 
-const PostHeader = () => {
-  const {onSubmit, isPending, canSubmit} = useDocumentWriteContext();
+type ModeProps = {
+  mode: 'post' | 'edit';
+};
+
+const RequestButton = ({mode}: ModeProps) => {
+  const values = useDocument(state => state.values);
+  const errors = useDocument(state => state.errorMessages);
+
+  const requiredFields: Array<ExcludeImages> = ['title', 'writer', 'contents'];
+  const canSubmit = requiredFields.every(field => values[field].trim() !== '' && errors[field] === null);
+
+  const {postDocument, isPostPending} = usePostDocument();
+  const {putDocument, isPutPending} = usePutDocument();
+  const isPending = isPostPending || isPutPending;
+
+  const onSubmit = async () => {
+    const newMetaList = await uploadImages({documentUUID: values.title, uploadImageMetaList: values.images});
+    const linkReplacedContents = replaceLocalUrlToS3Url(values.contents, newMetaList);
+
+    const document: PostDocumentContent = {
+      uuid: '',
+      title: values.title,
+      contents: linkReplacedContents,
+      writer: values.writer,
+      documentBytes: getBytes(linkReplacedContents),
+    };
+
+    if (mode === 'post') postDocument(document);
+    if (mode === 'edit') putDocument(document);
+  };
+
+  return (
+    <Button style="primary" size="xs" disabled={!canSubmit || isPending} onClick={onSubmit}>
+      작성완료
+    </Button>
+  );
+};
+
+const PostHeader = ({mode}: ModeProps) => {
   const router = useRouter();
 
   const goBack = () => {
@@ -14,15 +57,13 @@ const PostHeader = () => {
   };
 
   return (
-    <header className="flex justify-between w-full">
+    <header className="flex w-full justify-between">
       <DocumentTitle title="작성하기" />
       <fieldset className="flex gap-2">
-        <Button style="tertiary" size="xs" onClick={goBack}>
+        <Button type="button" style="tertiary" size="xs" onClick={goBack}>
           취소하기
         </Button>
-        <Button style="primary" size="xs" onClick={onSubmit} disabled={isPending || !canSubmit}>
-          작성완료
-        </Button>
+        <RequestButton mode={mode} />
       </fieldset>
     </header>
   );
