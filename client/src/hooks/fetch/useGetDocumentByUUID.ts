1cb4401b732dd40b23aa4f57c62710709ef34a45
김진호 <rlawlsgh1227@gmail.com>
2025-06-29 13:15:14 +0900

    refactor: getDocumentByUUID requestGetClient 메서드를 사용하도록 변경

--- client/src/hooks/fetch/useGetDocumentByUUID.ts
+++ client/src/hooks/fetch/useGetDocumentByUUID.ts
@@ -2,26 +2,21 @@
 
 import {ENDPOINT} from '@constants/endpoint';
 import {useFetch} from '@hooks/useFetch';
+import {requestGetClient} from '@http/client';
 import {WikiDocument} from '@type/Document.type';
 import {useCallback} from 'react';
 
 export const useGetDocumentByUUID = (uuid: string) => {
-  // TODO: 현재는 백엔드 서버로 직접 호출, 서버 -> 서버로 호출하는 방식으로 변경 필요 (현재는 client->server로 호출 중)
-  const getDocumentByUUID = async (uuid: string): Promise<WikiDocument> => {
-    const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}${ENDPOINT.getDocumentByUUID}/${uuid}`;
-    const response = await fetch(url);
+  const getDocumentByUUID = async (uuid: string) => {
+    const response = await requestGetClient<WikiDocument>({
+      baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+      endpoint: `${ENDPOINT.getDocumentByUUID}/${uuid}`,
+    });
 
-    if (!response.ok) {
-      const errorData = await response.json();
-      const errorMessage = errorData?.message || JSON.stringify(errorData) || 'API 요청 실패';
-      throw new Error(errorMessage);
-    }
-
-    return response.json() as Promise<WikiDocument>;
+    return response;
   };
 
   const getData = useCallback(() => getDocumentByUUID(uuid), [uuid]);
-
   const {data} = useFetch<WikiDocument>(getData);
 
   return {
