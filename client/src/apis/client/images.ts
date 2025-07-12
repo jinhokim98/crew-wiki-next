2c2c871c7637b5f4f25b99ea7235e18f8e2bc449
김진호 <rlawlsgh1227@gmail.com>
2025-07-12 22:06:18 +0900

    feat: 이미지 fetch 에러 상황 보강 uuid가 비었을 때, s3 이미지 업로드 실패 시 에러를 throw하도록

--- client/src/apis/client/images.ts
+++ client/src/apis/client/images.ts
@@ -14,7 +14,7 @@ const uploadImageToS3 = async (uploadImageKey: string, image: File) => {
     endpoint: `${ENDPOINT.getPresignedUrl}/${uploadImageKey}`,
   });
 
-  await fetch(url, {
+  const response = await fetch(url, {
     method: 'PUT',
     body: image,
     headers: {
@@ -23,6 +23,10 @@ const uploadImageToS3 = async (uploadImageKey: string, image: File) => {
       'x-amz-meta-content-type': 'image/jpeg',
     },
   });
+
+  if (!response.ok) {
+    throw new Error('이미지 업로드 중에 문제가 발생했습니다. 새로고침 후 다시 시도해주세요.');
+  }
 };
 
 const resizeFile = (file: File): Promise<File> =>
@@ -51,11 +55,15 @@ export async function uploadImage(documentUUID: string, imageFile: File) {
   const extension = resizedImage.type.split('/')[1];
   const uploadImageKey = `${documentUUID}/${randomFileName}.${extension}`;
 
+  if (documentUUID === '') {
+    throw new Error('이미지 업로드 중에 문제가 발생했습니다. 새로고침 후 다시 시도해주세요.');
+  }
+
   try {
     await uploadImageToS3(uploadImageKey, resizedImage);
   } catch (err) {
-    console.error('이미지 업로드에 실패했습니다.', err);
-    throw new Error('이미지 업로드에 실패했습니다.');
+    console.error('S3 이미지 업로드 실패', err);
+    throw new Error('이미지 업로드 중에 문제가 발생했습니다. 새로고침 후 다시 시도해주세요.');
   }
 
   return `${process.env.NEXT_PUBLIC_IMAGE_CLOUDFRONT_DOMAIN}/${uploadImageKey}`;
