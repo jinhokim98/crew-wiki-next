aedbec3335cfb88351184ac23a778d0e8c1e1a8d
김진호 <rlawlsgh1227@gmail.com>
2025-07-12 21:59:25 +0900

    feat: useMutation의 onMutate, onSettled 옵션 추가

--- client/src/hooks/useMutation.ts
+++ client/src/hooks/useMutation.ts
@@ -3,39 +3,47 @@
 import {useState} from 'react';
 
 type MutationFn<TVariables, TData> = (variables: TVariables) => Promise<TData>;
-type OnSuccess<TData> = (data: TData) => void;
-type OnError = (error: unknown) => void;
+type OnMutate<TVariables> = (variables: TVariables) => void;
+type OnSuccess<TData, TVariables> = (data: TData, variables: TVariables) => void;
+type OnError<TVariables> = (error: unknown, variables: TVariables) => void;
+type OnSettled<TData, TVariables> = (data: TData, variables: TVariables) => void;
 
 interface UseMutationArgs<TVariables, TData> {
   mutationFn: MutationFn<TVariables, TData>;
-  onSuccess?: OnSuccess<TData>;
-  onError?: OnError;
+  onMutate?: OnMutate<TVariables>;
+  onSuccess?: OnSuccess<TData, TVariables>;
+  onError?: OnError<TVariables>;
+  onSettled?: OnSettled<TData | null, TVariables>;
 }
 
 const useMutation = <TVariables = void, TData = unknown>({
   mutationFn,
+  onMutate,
   onSuccess,
   onError,
+  onSettled,
 }: UseMutationArgs<TVariables, TData>) => {
   const [isPending, setIsPending] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [data, setData] = useState<TData | null>(null);
 
   const mutate = async (variables: TVariables) => {
+    if (onMutate) onMutate(variables);
     setIsPending(true);
     setErrorMessage(null);
 
     try {
       const data = await mutationFn(variables);
       setData(data);
-      if (onSuccess) onSuccess(data);
+      if (onSuccess) onSuccess(data, variables);
     } catch (err) {
       if (err instanceof Error) {
         setErrorMessage(err.message);
-        if (onError) onError(err);
+        if (onError) onError(err, variables);
       }
     } finally {
       setIsPending(false);
+      if (onSettled) onSettled(data, variables);
     }
   };
 
